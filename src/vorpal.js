11a12fef3c81f3d06aa14aa4d7dcd2ea62379c8f
wuruipeng <1256807020@qq.com>
2020-08-24 22:42:54 +0800

    格式化展示命令行数据

--- src/vorpal.js
+++ src/vorpal.js
@@ -1,15 +1,32 @@
 const vorpal = require('vorpal')()
 const Blockchain = require('./blockchain')
 const blockchain = new Blockchain()
+const Table = require('cli-table')
+function formatLog(data) {
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  const first = data[0]
+  const head = Object.keys(first)
+  const table = new Table({
+    head: head,
+    colWidths: new Array(head.length).fill(20)
+  })
+  const res = data.map(v => {
+    return head.map(h => v[h])
+  })
+  table.push(...res)
+  console.log(table.toString())
+}
 vorpal.command('mine', '挖矿').action(function (args, callback) {
   const newBlock = blockchain.mine()
   if (newBlock) {
-    console.log(newBlock)
+    formatLog(newBlock)
   }
   callback()
 })
 vorpal.command('chain', '查看区块链').action(function (args, callback) {
-  this.log(blockchain.blockchain)
+  formatLog(blockchain.blockchain)
   callback()
 })
 console.log('welcome')
